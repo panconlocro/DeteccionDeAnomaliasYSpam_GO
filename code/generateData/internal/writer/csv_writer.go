f4d13b62dd8ca2220e4a9cba1c80c41859fdf0af
panconlocro <rosamariarv10@gmail.com>
2026-05-08 14:03:34 -0500

    feat: escritura del csv

---
+++ code/generateData/internal/writer/csv_writer.go
@@ -0,0 +1,188 @@
+// package writer
+
+// import (
+//     "encoding/csv"
+//     "os"
+// 	"path/filepath" 
+//     "strings"
+
+//     "detecciondeanomalias/code/generateData/internal/model"
+// )
+
+// type CSVWriter struct {
+//     Writer *csv.Writer
+// 	File   *os.File
+// }
+
+// func NewCSVWriter(path string) (*CSVWriter, error) {
+
+// 	dir := filepath.Dir(path)
+
+// 	err := os.MkdirAll(dir, 0755)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+//     f, err := os.Create(path)
+//     if err != nil {
+//         return nil, err
+//     }
+
+//     w := csv.NewWriter(f)
+
+// 	header := []string{
+// 		"NRO_EXPEDIENTE",
+// 		"EXPEDIENTE_ORIGEN_pres",
+// 		"TIPO_EXPEDIENTE_pres",
+// 		"FECHA_PRESENTACION_pres",
+// 		"DENUNCIADOS_pres",
+// 		"DOC_DENUNCIADO",
+// 		"MATERIA_pres",
+// 		"año_pres",
+// 		"FECHA_RESOLUCION",
+// 		"NRO_RESOLUCION",
+// 		"FORMA_CONCLUSION",
+// 		"año_res",
+// 		"RES_MATCH_SOURCE",
+
+// 		// synthetic
+// 		"DETALLE_QUEJA",
+// 		"ES_SPAM",
+// 		"SPAM_TAGS",
+// 		"SPAM_SCORE",
+// 		"CAMPAIGN_ID",
+// 	}
+
+//     w.Write(header)
+
+//     return &CSVWriter{
+//         Writer: w,
+//     }, nil
+// }
+
+// func (w *CSVWriter) Write(c model.Complaint) error {
+
+//     row := []string{
+//         c.ID,
+//         c.FechaHora.Format("2006-01-02 15:04:05"),
+//         c.Denunciado,
+//         c.Detalle,
+//         boolToString(c.EsSpam),
+//         strings.Join(c.SpamTags, ";"),
+//     }
+
+//     return w.Writer.Write(row)
+// }
+
+// func boolToString(v bool) string {
+//     if v {
+//         return "1"
+//     }
+//     return "0"
+// }
+
+package writer
+
+import (
+	"encoding/csv"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"detecciondeanomalias/code/generateData/internal/model"
+)
+
+type CSVWriter struct {
+
+	Writer *csv.Writer
+
+	OriginalColumns []string
+}
+
+func NewCSVWriter(
+	path string,
+	originalColumns []string,
+) (*CSVWriter, error) {
+
+	f, err := os.Create(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	w := csv.NewWriter(f)
+
+	sort.Strings(originalColumns)
+
+	header := append(
+		originalColumns,
+
+		"TIMESTAMP",
+		"DETALLE_QUEJA",
+		"ES_SPAM",
+		"SPAM_TAGS",
+		"SPAM_SCORE",
+	)
+
+	err = w.Write(header)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &CSVWriter{
+		Writer:          w,
+		OriginalColumns: originalColumns,
+	}, nil
+}
+
+func (w *CSVWriter) Write(
+	c model.Complaint,
+) error {
+
+	row := []string{}
+
+	for _, col := range w.OriginalColumns {
+
+		row = append(
+			row,
+			c.OriginalData[col],
+		)
+	}
+
+	row = append(
+		row,
+
+		c.Timestamp.Format(
+			"2006-01-02 15:04:05",
+		),
+
+		c.DetalleQueja,
+
+		boolToString(c.EsSpam),
+
+		strings.Join(
+			c.SpamTags,
+			";",
+		),
+
+		strconv.FormatFloat(
+			c.SpamScore,
+			'f',
+			2,
+			64,
+		),
+	)
+
+	return w.Writer.Write(row)
+}
+
+func boolToString(v bool) string {
+
+	if v {
+		return "1"
+	}
+
+	return "0"
+}
